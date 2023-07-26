18d7468a5ac2a1e1301a89114caeb9f587c4ff46
smuushi <mishih780@gmail.com>
2023-07-26 16:22:26 -0400

    asdf

---
+++ backend/models/Poster.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const posterSchema = new Schema(
+    {
+        script: {
+            type: String
+        },
+
+        AWSKey: {
+            type: String, 
+            required: true
+        },
+        email: {
+            type: String
+        }
+    }
+
+)
+
+
+
+module.exports = mongoose.model("Image", imageSchema);
