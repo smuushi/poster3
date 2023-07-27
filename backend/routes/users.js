4c956eafe5754ecd3275dcabe6868af681547b8e
YLinDev <adikit1@icloud.com>
2023-07-26 22:29:08 -0400

    mongoDB connection

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Respond with a user resource Yong');
 });
 
 module.exports = router;
